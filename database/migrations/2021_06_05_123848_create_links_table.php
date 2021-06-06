74305c465d8a94b52c684735b0d9354698a58bd7
Recep <16542010@firat.edu.tr>
2021-06-06 20:31:31 +0300

    lcoation 2

---
+++ database/migrations/2021_06_05_123848_create_links_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('global_id')->nullable()->default(null);
+            $table->foreign('global_id')->references('id')->on('global');
+
+            $table->string('url')->default(null);
+            $table->boolean('status')->default(true);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+}
